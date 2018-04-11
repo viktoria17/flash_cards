ad11f66b6fdcbfa33741ec63e34f4c06e37d4cd2
Viktoria Gužovskaja <viktoria.guzovskaja@gmail.com>
2018-04-11 20:44:09 +0300

    Handle 404 error

--- app/app.js
+++ app/app.js
@@ -58,6 +58,18 @@ app.post('/goodbye', (req, res) => {
 	res.redirect('/hello')
 });
 
+app.use((req, res, next) => {
+	const err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+  
+app.use((err, req, res, next) => {
+	res.locals.error = err;
+	res.status(err.status);
+	res.render('error', err);
+});
+
 app.listen(3000, () => {
 	console.log('The application is running on localhost:3000');
 });
