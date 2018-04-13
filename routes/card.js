76c27d201ddb935d00414b40b1b88837ee6d09c8
Viktoria Gužovskaja <viktoria.guzovskaja@gmail.com>
2018-04-13 19:24:27 +0300

    Usa data and route parameters

--- routes/card.js
+++ routes/card.js
@@ -3,9 +3,13 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
-router.get('/', (req, res) => {
+router.get('/:id', (req, res) => {
 	// every route in this file starts with /card, because we are directing traffic into this file from the card path in the app.js file
-	res.render('card', { question: 'What is always coming, but never arrives?'});
+	res.render('card', { 
+		question: cards[req.params.id].question,
+		hint: cards[req.params.id].hint,
+	});
+	console.log(req.params.id)
 });
 
 module.exports = router;
