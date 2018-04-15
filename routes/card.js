a72bb424b02f19ec5422add3b8ddad5521b5c824
Viktoria Gužovskaja <viktoria.guzovskaja@gmail.com>
2018-04-15 12:59:01 +0300

    Improve the application's lnks

--- routes/card.js
+++ routes/card.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
+router.get('/', (req, res) => {
+	const cardsAmount = cards.length;
+	const cardRandomNr = Math.floor(Math.random() * cardsAmount);
+	res.redirect(`/card/${cardRandomNr}`);
+});
+
 router.get('/:id', (req, res) => {
 	// http://localhost:3000/card/1?side=question
 	const { side } = req.query;
@@ -11,16 +17,25 @@ router.get('/:id', (req, res) => {
 	const { id } = req.params; 
 	console.log('id: ', id); // => 1
 
+	if (!side) {
+		res.redirect(`/card/${id}?side=question`);
+	}
+
 	const text = cards[id][side];
 	console.log('question: ', text); // => What is one way a website can store data in a user's browser?
 
-	const hint = cards[id].hint;
+	const { hint } = cards[id];
 	console.log('hint: ', hint); // => They are delicious with milk
 
-	const templateData = { text };
+	const templateData = { id, text };
 
 	if (side === 'question') {
 		templateData.hint = hint;
+		templateData.sideToShow = 'answer';
+		templateData.sideToShowDisplay = 'Answer';
+	} else if (side === 'answer') {
+		templateData.sideToShow = 'question';
+		templateData.sideToShowDisplay = 'Question';
 	}
 
 	res.render('card', templateData);
